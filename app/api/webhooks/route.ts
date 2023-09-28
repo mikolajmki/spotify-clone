68804e89ce6acfdd5264998da7fc1c45402a0cd1
mikolajmki <mikol4m@gmail.com>
2023-09-28 16:49:09 +0200

    stripe integration

---
+++ app/api/webhooks/route.ts
@@ -0,0 +1,17 @@
+import Stripe from "stripe";
+import { NextResponse } from "next/server";
+import { headers } from "next/headers";
+
+import { stripe } from "@/libs/stripe";
+import { upsertProductRecord, upsertPriceRecord, manageSubscriptionStatusChange } from "@/libs/supabaseAdmin";
+
+const relevantEvents = new Set([
+    "product_created",
+    "product.updated",
+    "price.created",
+    "price.updated",
+    "checkout.session.completed",
+    "customer.subscription.created",
+    "customer.subscription.updated",
+    "customer.subscription.deleted"
+])
